6a83ff091068415857159f2a49e184c9b11a0d43
Mercedez Castro <mercedez@hawaii.edu>
2022-02-17 20:01:26 -0800

    add reddit scraper app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Reddit Scraper for Agile Garden Club
+
+Simple Reddit Scraper to obtain results from a subreddit. Configure as needed to suit your purposes.
+
+## Installation
+
+### Obtaining Reddit API Key
+
+Obtain a `REDDIT_CLIENT_ID` and `REDDIT_CLIENT_SECRET` by [following Reddit's official instructions for setting up a Reddit application](https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps)
+
+Create a `.env` file and fill in the corresponding values from `.env.template`
+
+### Install Python Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running
+
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
+import praw
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+
+    subreddit: str = os.getenv("REDDIT_SUBREDDIT")
+    db_filepath = f"{subreddit}.csv"
+
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+        user_agent=os.getenv("REDDIT_USER_AGENT"),
+    )
+
+    submissions = [
+        submission for submission in tqdm(reddit.subreddit(subreddit).new(limit=1000))
+    ]
+
+    def submission_to_record(s):
+        return (s.title, str(datetime.fromtimestamp(s.created_utc)), s.url)
+
+    submission_to_record(submissions[0])
+
+    records = [submission_to_record(s) for s in submissions]
+    df = pd.DataFrame(records, columns=["title", "date", "url"])
+
+    df.to_csv(db_filepath)
